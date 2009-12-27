424ce1de42a43823c2ae4d2cc9690fc00633a9f5
Ben Hoskings <ben@hoskings.net>
2009-12-27 21:53:37 +1100

    Split 'passenger deploy repo' into exists and hook.

--- git.rb
+++ git.rb
@@ -1,13 +1,25 @@
 dep 'passenger deploy repo' do
+  requires 'passenger deploy repo exists', 'passenger deploy repo hook'
+end
+
+dep 'passenger deploy repo hook' do
+  requires 'passenger deploy repo exists'
+  met? { (var(:passenger_repo_root) / '.git/hooks/post-receive').executable? }
+  meet {
+    in_dir var(:passenger_repo_root), :create => true do
+      render_erb "git/deploy-repo-post-receive", :to => '.git/hooks/post-receive'
+      shell "chmod +x .git/hooks/post-receive"
+    end
+  }
+end
+
+dep 'passenger deploy repo exists' do
   requires 'git', 'user exists'
   define_var :passenger_repo_root, :default => :rails_root
-  met? { File.directory? pathify var(:passenger_repo_root) / '.git' }
+  met? { (var(:passenger_repo_root) / '.git').dir? }
   meet {
-    FileUtils.mkdir_p pathify var(:passenger_repo_root) and
-    in_dir var(:passenger_repo_root) do
+    in_dir var(:passenger_repo_root), :create => true do
       shell "git init"
-      render_erb "git/deploy-repo-post-receive", :to => pathify(var(:passenger_repo_root) / '.git/hooks/post-receive')
-      shell "chmod +x #{pathify var(:passenger_repo_root) / '.git/hooks/post-receive'}"
     end
   }
 end
