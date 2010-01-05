dfb49afd19c7ed240ef93df5181760248fee7b45
Ben Hoskings <ben@hoskings.net>
2010-01-05 15:33:04 +1100

    Sudo when appending to /etc/shells.

--- user.rb
+++ user.rb
@@ -7,7 +7,7 @@ end
 dep 'fish' do
   requires 'fish installed'
   met? { grep which('fish'), '/etc/shells' }
-  meet { append_to_file which('fish'), '/etc/shells' }
+  meet { append_to_file which('fish'), '/etc/shells', :sudo => true }
 end
 
 src 'fish installed' do
