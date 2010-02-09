f9fec14b256abd3ab969ba9a09610fcf3f3e5c17
Ben Hoskings <ben@hoskings.net>
2010-02-09 23:32:15 +1100

    Added TextMate, Fluid and RubyMine app deps.

---
+++ app_bundles.rb
@@ -0,0 +1,11 @@
+app 'TextMate.app' do
+  source 'http://download-b.macromates.com/TextMate_1.5.9.dmg'
+end
+
+app 'Fluid.app' do
+  source 'http://fluidapp.com/dist/Fluid_0.9.6.zip'
+end
+
+app 'RubyMine.app' do
+  source 'http://download.jetbrains.com/ruby/rubymine-2.0.1.dmg'
+end
