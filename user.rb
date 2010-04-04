03e33fa21a6c63d7015a168c49df2c82a82bc84d
Ben Hoskings <ben@hoskings.net>
2010-04-04 20:32:13 +1000

    Added 'user exists' / meet{} for OS X.

--- user.rb
+++ user.rb
@@ -38,6 +38,23 @@ end
 dep 'user exists' do
   on :osx do
     met? { !shell("dscl . -list /Users").split("\n").grep(var(:username)).empty? }
+    meet {
+      homedir = var(:home_dir_base) / var(:username)
+      {
+        'Password' => '*',
+        'UniqueID' => (501...1024).detect {|i| (Etc.getpwuid i rescue nil).nil? },
+        'PrimaryGroupID' => 'admin',
+        'RealName' => var(:username),
+        'NFSHomeDirectory' => homedir,
+        'UserShell' => '/dev/null'
+      }.each_pair {|k,v|
+        # /Users/... here is a dscl path, not a filesystem path.
+        sudo "dscl . -create #{'/Users' / var(:username)} #{k} '#{v}'"
+      }
+      sudo "mkdir -p '#{homedir}'"
+      sudo "chown #{var(:username)}:admin '#{homedir}'"
+      sudo "chmod 701 '#{homedir}'"
+    }
   end
   on :linux do
     met? { grep(/^#{var(:username)}:/, '/etc/passwd') }
