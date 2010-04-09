13c331d278580cd567c3ff6636678a8d144709e5
Ben Hoskings <ben@hoskings.net>
2010-04-09 15:47:34 +1000

    Added sphinx dep.

---
+++ sphinx.rb
@@ -0,0 +1,10 @@
+src 'sphinx' do
+  source 'http://www.sphinxsearch.com/downloads/sphinx-0.9.9.tar.gz'
+  provides 'search', 'searchd', 'indexer'
+  configure_args L{
+    [
+      ("--with-pgsql=#{shell 'pg_config --pkgincludedir'}" if confirm("Build with postgres support?")),
+      ("--without-mysql" unless confirm("Build with mysql support?"))
+    ].compact.join(" ")
+  }
+end
