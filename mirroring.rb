052e083b6c26a563ba5dee06fddc339e179b507b
Ben Hoskings <ben@hoskings.net>
2010-04-11 18:51:29 +1000

    Added 'mirror has assets'.

---
+++ mirroring.rb
@@ -0,0 +1,22 @@
+dep 'mirror has assets' do
+  helper :assets do
+    var(:mirror_path).glob("**/*").select {|f|
+      f[/html?|css/i]
+    }.map {|f|
+      f.p.read.scan(/url\(['"]?([^)'"]+)['"]?\)/).map {|url|
+        url.starts_with?('/') ? url : (f.p.dirname / url).to_s.gsub('/Users/ben/projects/corkboard/current/public', '')
+      }
+    }.flatten
+  end
+  helper :nonexistent_assets do
+    assets.reject {|asset|
+      (var(:mirror_path) / asset).exists?
+    }
+  end
+  met? { nonexistent_assets.empty? }
+  meet {
+    nonexistent_assets.each {|asset|
+      log_shell "Downloading #{asset}", "wget -O '#{var(:mirror_path) / asset}' '#{asset}'"
+    }
+  }
+end
