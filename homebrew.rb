2a5d51d5a185f6cb2e81ff6f40ef02dd8fd2b38a
Ben Hoskings <ben@hoskings.net>
2010-04-11 22:16:08 +1000

    Added homebrew mirroring deps.

---
+++ homebrew.rb
@@ -0,0 +1,69 @@
+meta :homebrew_mirror do
+  template {
+    helper :urls do
+      script = %Q{
+#!/usr/bin/env ruby
+
+prefix = `brew --prefix`.chomp
+$LOAD_PATH << File.join(prefix, 'Library', 'Homebrew')
+
+require 'extend/ARGV'
+require 'global'
+ARGV.extend HomebrewArgvExtension
+require 'formula'
+
+before = Class.constants
+
+Dir[File.join(prefix, 'Library/Formula/*')].each {|f| load f }
+classes_to_skip = %w[AspellLang COREUTILS_ALIASES DICT_CONF Rational SOLR_START_SCRIPT]
+
+urls = (Class.constants - before - classes_to_skip).reject {|k|
+  k =~ /DownloadStrategy$/
+}.map {|k|
+  eval(k).new
+}.select {|k|
+  k.respond_to? :url
+}.map {|k|
+  k.url
+}
+puts urls * "\n"
+      }
+      shell("ruby", :input => script).split("\n").select {|url| url[/^(https?|ftp):/] }
+    end
+  }
+end
+
+homebrew_mirror 'homebrew mirrored' do
+  define_var :homebrew_downloads, :default => '/srv/http/files'
+  define_var :homebrew_vhost_root, :default => '/srv/http/homebrew'
+  helper :missing_urls do
+    urls.tap {|urls| log "#{urls.length} URLs in the homebrew database." }.reject {|url|
+      path = var(:homebrew_downloads) / File.basename(url)
+      path.exists? && !path.empty?
+    }.tap {|urls| log "Of those, #{urls.length} aren't present locally." }
+  end
+  met? { missing_urls.empty? }
+  meet {
+    in_dir var(:homebrew_downloads) do
+      missing_urls.each {|url| Babushka::Archive.download url }
+    end
+  }
+end
+
+homebrew_mirror 'homebrew linked' do
+  requires 'homebrew mirrored'
+  helper :unlinked_urls do
+    urls.select {|url|
+      path = var(:homebrew_downloads) / File.basename(url)
+      link = var(:homebrew_vhost_root) / url.sub(/^[a-z]+:\/\/[^\/]+\//, '')
+      path.exists? && !(link.exists? && link.readlink)
+    }
+  end
+  met? { unlinked_urls.empty? }
+  meet {
+    unlinked_urls.each {|url|
+      shell "mkdir -p '#{var(:homebrew_vhost_root) / File.dirname(url.sub(/^[a-z]+:\/\/[^\/]+\//, ''))}'"
+      log_shell "Linking #{url}", "ln -sf '#{var(:homebrew_downloads) / File.basename(url)}' '#{var(:homebrew_vhost_root) / url.sub(/^[a-z]+:\/\/[^\/]+\//, '')}'"
+    }
+  }
+end
