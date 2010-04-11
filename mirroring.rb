67caa6ff6151364049e58049f59d99ae307915aa
Ben Hoskings <ben@hoskings.net>
2010-04-11 19:12:52 +1000

    Don't treat mirror_domain as a local path.

--- mirroring.rb
+++ mirroring.rb
@@ -18,7 +18,7 @@ dep 'mirror has assets' do
   meet {
     nonexistent_assets.each {|asset|
       shell "mkdir -p '#{var(:mirror_path) / asset.p.dirname}'"
-      log_shell "Downloading #{asset}", "wget -O '#{var(:mirror_path) / asset}' '#{var(:mirror_domain) / asset}'"
+      log_shell "Downloading #{asset}", "wget -O '#{var(:mirror_path) / asset}' '#{File.join var(:mirror_domain), asset}'"
     }
   }
 end
