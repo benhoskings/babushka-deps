327c75d0cf9ba3b65b38c5bc2141a78f35ced8ad
Ben Hoskings <ben@hoskings.net>
2010-07-21 08:52:46 +1000

    Fixed nginx references in 'webapp'.

--- top level.rb
+++ top level.rb
@@ -28,7 +28,7 @@ dep 'proxied app' do
 end
 
 dep 'webapp' do
-  requires 'user exists', 'vhost enabled', 'webserver running'
+  requires 'user exists', 'vhost enabled.nginx', 'webserver running.nginx'
   define_var :domain, :default => :username
   setup {
     set :home_dir_base, "/srv/http"
