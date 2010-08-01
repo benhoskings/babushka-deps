852b41e63db73de32d9f69643c60dbdabe3e6e5b
Ben Hoskings <ben@hoskings.net>
2010-08-01 14:55:57 +1000

    Added TODO about vendoring gems during `bundle install`.

--- bundler.rb
+++ bundler.rb
@@ -2,6 +2,10 @@ dep 'app bundled' do
   requires 'deployed app', 'bundler.gem'
   met? { in_dir(var(:rails_root)) { shell 'bundle check', :log => true } }
   meet { in_dir(var(:rails_root)) { shell 'bundle install --without test', :log => true } }
+  # TODO maybe vendor the gems by default? Depends if gem partitioning is
+  # required, or if bundler's version requirement resolution is good enough
+  # that it doesn't matter.
+  # `bundle install ./vendor --disable-shared-gems`
 end
 
 dep 'deployed app' do
