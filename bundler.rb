54662308b00002f26f27cbdcda078b6dcda5bfef
Ben Hoskings <ben@hoskings.net>
2010-09-27 14:08:12 +1000

    When bundling in developemnt, don't exclude dev & test, and don't vendor.

--- bundler.rb
+++ bundler.rb
@@ -1,7 +1,13 @@
 dep 'app bundled' do
   requires 'deployed app', 'bundler.gem'
   met? { in_dir(var(:rails_root)) { shell 'bundle check', :log => true } }
-  meet { in_dir(var(:rails_root)) { shell 'bundle install --without development --without test --path ./vendor', :log => true } }
+  meet { in_dir(var(:rails_root)) {
+    if var(:rails_env) == 'production'
+      shell 'bundle install --without development --without test --path ./vendor', :log => true
+    else
+      shell 'bundle install', :log => true
+    end
+  } }
 end
 
 dep 'deployed app' do
