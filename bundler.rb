db0928d6be11a72a0ee6cdcdcfd13f337dcd5296
Ben Hoskings <ben@hoskings.net>
2010-10-05 23:51:02 +1100

    Optionally try a `bundle update` if the `bundle install` doesn't work.

--- bundler.rb
+++ bundler.rb
@@ -2,10 +2,11 @@ dep 'app bundled' do
   requires 'deployed app', 'bundler.gem'
   met? { in_dir(var(:rails_root)) { shell 'bundle check', :log => true } }
   meet { in_dir(var(:rails_root)) {
-    if var(:rails_env) == 'production'
-      shell 'bundle install --without development --without test --path ./vendor', :log => true
-    else
-      shell 'bundle install', :log => true
+    install_args = var(:rails_env) != 'production' ? '' : '--without development --without test --path ./vendor'
+    unless shell("bundle install #{install_args}", :log => true)
+      confirm("Try a `bundle update`") {
+        shell 'bundle update', :log => true
+      }
     end
   } }
 end
