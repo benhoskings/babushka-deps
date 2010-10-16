6626cf79a4d49b4c6284cc139fea3c82b0649348
Ben Hoskings <ben@hoskings.net>
2010-10-16 16:19:26 +1100

    In 'hostname', add the unqualified hostname as an alias for 127.0.0.1 as well as the full hostname.

--- system.rb
+++ system.rb
@@ -12,7 +12,7 @@ dep 'hostname', :for => :linux do
   }
   meet {
     sudo "echo #{var :hostname, :default => shell('hostname')} > /etc/hostname"
-    sudo "sed -ri 's/^127.0.0.1.*$/127.0.0.1 #{var :hostname} localhost.localdomain localhost/' /etc/hosts"
+    sudo "sed -ri 's/^127.0.0.1.*$/127.0.0.1 #{var(:hostname)} #{var(:hostname).sub(/\..*$/, '')} localhost.localdomain localhost/' /etc/hosts"
     sudo "hostname #{var :hostname}"
   }
 end
