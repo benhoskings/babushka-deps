9685d1fb403877dddfc9d6d15ddbc5c68ed6c95e
Ben Hoskings <ben@hoskings.net>
2010-11-26 10:54:24 +1100

    Added 'github token set'.

--- git.rb
+++ git.rb
@@ -35,3 +35,8 @@ dep 'passenger deploy repo exists' do
     end
   }
 end
+
+dep 'github token set' do
+  met? { !shell('git config --global github.token').blank? }
+  meet { shell("git config --global github.token '#{var(:github_token)}'")}
+end
