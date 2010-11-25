3a195337386fee4e13aa70aaac02b2cac9c6157d
Ben Hoskings <ben@hoskings.net>
2010-11-26 10:53:51 +1100

    Update 'bundle install' command to use --deployment and new --without syntax.

--- bundler.rb
+++ bundler.rb
@@ -2,7 +2,7 @@ dep 'app bundled' do
   requires 'deployed app', 'bundler.gem', 'db gem'
   met? { in_dir(var(:rails_root)) { shell 'bundle check', :log => true } }
   meet { in_dir(var(:rails_root)) {
-    install_args = var(:rails_env) != 'production' ? '' : '--without development --without test --path ./vendor'
+    install_args = var(:rails_env) != 'production' ? '' : "--deployment --without 'development test'"
     unless shell("bundle install #{install_args}", :log => true)
       confirm("Try a `bundle update`") {
         shell 'bundle update', :log => true
