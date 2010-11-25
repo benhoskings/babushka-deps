c9033333ecefd500f42e0f3b3d3c7e053850dc02
Ben Hoskings <ben@hoskings.net>
2010-11-26 10:53:07 +1100

    Use passenger ~> 3.0.

--- packages.rb
+++ packages.rb
@@ -57,7 +57,7 @@ end
 dep 'nmap.managed'
 dep 'oniguruma.managed'
 dep 'passenger.gem' do
-  installs 'passenger' => '>= 3.0.0.pre4'
+  installs 'passenger' => '~> 3.0'
   provides 'passenger-install-nginx-module'
 end
 dep 'pcre.managed' do
