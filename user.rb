78ee7f317121971ca23fcee1daf3dcb4ef721de4
Ben Hoskings <ben@hoskings.net>
2010-12-01 10:15:02 +1100

    Run both commands in 'passwordless ssh logins' / before {} with sudo.

--- user.rb
+++ user.rb
@@ -1,7 +1,10 @@
 dep 'passwordless ssh logins' do
   helper(:ssh_dir) { var(:home_dir_base) / var(:username) / '.ssh' }
   met? { sudo "grep '#{var(:your_ssh_public_key)}' '#{ssh_dir / 'authorized_keys'}'" }
-  before { sudo "mkdir -p '#{ssh_dir}'; chmod 700 '#{ssh_dir}'" }
+  before {
+    sudo "mkdir -p '#{ssh_dir}'"
+    sudo "chmod 700 '#{ssh_dir}'"
+  }
   meet { append_to_file var(:your_ssh_public_key), (ssh_dir / 'authorized_keys'), :sudo => true }
   after {
     shell "chown -R #{var(:username)}:#{var(:username)} '#{ssh_dir}'"
