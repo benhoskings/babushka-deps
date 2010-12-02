4fa48b4000c9b033ca8f2700dda251100e47e028
Ben Hoskings <ben@hoskings.net>
2010-12-03 00:02:22 +1100

    Added some locale deps.

---
+++ locale.rb
@@ -0,0 +1,28 @@
+meta :locale do
+  template {
+    helper :locale_regex do
+      /[\w]+\.utf8/
+    }
+    helper :local_locale do
+      shell('locale -a').split("\n").detect {|l|
+        l[locale_regex]
+      }
+    end
+  }
+end
+
+dep 'set.locale' do
+  requires 'locale exists'
+  met? {
+    shell('locale').val_for('LANG')[locale_regex]
+  }
+  on :apt do
+    meet {
+      sudo("echo 'LANG=#{local_locale}' > /etc/default/locale")
+    }
+  end
+end
+
+dep 'exists.locale' do
+  met? { local_locale }
+end
