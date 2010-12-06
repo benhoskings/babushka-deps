28bb8a5f9de585667a8faa2788c6a3220fa7bf1a
Ben Hoskings <ben@hoskings.net>
2010-12-07 09:26:52 +1100

    Fixed ruby 1.9.2p0 source URL.

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,5 @@
 dep 'ruby19.src' do
-  source 'ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p0.tar.gz'
+  source 'ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p0.tar.gz'
   provides 'ruby19'
   configure_args '--program-suffix=19'
 end
