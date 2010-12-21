e88ee5d810548e9d23fd15becf4417697243524d
Ben Hoskings <ben@hoskings.net>
2010-12-21 18:40:27 +1100

    Added ree.src.

---
+++ deb.rb
@@ -0,0 +1,7 @@
+dep 'ree.src' do
+  source "http://rubyforge.org/frs/download.php/71098/ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb"
+  process_source {
+    sudo("dpkg -i ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb")
+  }
+  met? { shell("ruby --version") =~ /#{Regexp.escape("ruby 1.8.7 (2010-04-19 patchlevel 253) [x86_64-linux], MBARI 0x6770, Ruby Enterprise Edition 2010.02")}/ }
+end
