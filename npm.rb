f40e2ac0b063f82d266a7686141fa12533ba56f2
Ben Hoskings <ben@hoskings.net>
2010-12-21 18:40:49 +1100

    Added 'npm mirrored' - not really tested or finished.

---
+++ npm.rb
@@ -0,0 +1,14 @@
+dep 'npm mirrored' do
+  define_var :npm_mirror_root, :default => '/srv/http/registry.npmjs.org'
+  registry = "http://registry.npmjs.org/"
+  met? {
+    require 'json'
+    JSON.parse(`curl #{registry} 2>/dev/null`).each_pair {|pkg,details|
+      details['versions'].each_pair {|version,url|
+        src = JSON.parse(`curl #{url} 2>/dev/null`)['dist']['tarball']
+        destdir = File.dirname(src.sub(/^[a-z]+:\/\/[^\/]+\//, ''))
+        Babushka::Resource.download src
+      }
+    }
+  }
+end
