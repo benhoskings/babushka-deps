ba05dcc1214cc5927b35b81f7b7afa300d103ff4
Ben Hoskings <ben@hoskings.net>
2010-12-21 14:59:53 +1100

    Only check for ruby, irb and gem in ruby19 src builds.

--- ruby.rb
+++ ruby.rb
@@ -1,13 +1,13 @@
 dep 'ruby trunk.src' do
   requires 'bison.managed', 'readline headers.managed'
   source 'git://github.com/ruby/ruby.git'
-  provides 'ruby == 1.9.3.dev', 'gem', 'irb', 'rake', 'rdoc', 'ri', 'testrb'
+  provides 'ruby == 1.9.3.dev', 'gem', 'irb'
 end
 
 dep 'ruby19.src' do
   requires 'readline headers.managed'
   source 'ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p0.tar.gz'
-  provides 'ruby == 1.9.2p0', 'gem', 'irb', 'rake', 'rdoc', 'ri', 'testrb'
+  provides 'ruby == 1.9.2p0', 'gem', 'irb'
   # TODO: hack for ruby bug where bin/* aren't installed when the build path
   # contains a dot-dir.
   install {
