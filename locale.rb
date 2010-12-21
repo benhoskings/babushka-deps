3b6670415bb501c4082cdc9b59afcef4c1fb91f5
Ben Hoskings <ben@hoskings.net>
2010-12-21 21:41:02 +1100

    Hardcore the locale to en_US for now.

--- locale.rb
+++ locale.rb
@@ -1,6 +1,7 @@
 meta :locale do
   def locale_regex
-    /[\w]+\.utf-?8/i
+    # TODO: would be nice to not hardcode to en_US.
+    /en_US\.utf-?8/i
   end
   def local_locale
     shell('locale -a').split("\n").detect {|l|
