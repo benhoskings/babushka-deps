168f2eb89c0358d22d06d2d35d41ac3e5673351e
Ben Hoskings <ben@hoskings.net>
2010-12-21 12:00:15 +1100

    Require readline headers in ruby19.trunk.src.

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,5 @@
 dep 'ruby19.trunk.src' do
-  requires 'bison.managed'
+  requires 'bison.managed', 'readline headers.managed'
   source 'git://github.com/ruby/ruby.git'
   configure_args '--program-suffix=19'
   provides %w[ruby19 gem19 irb19 rake19 rdoc19 ri19 testrb19]
