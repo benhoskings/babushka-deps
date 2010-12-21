503388b16405e3b3f34366d081a8e39a8d1a4ff2
Ben Hoskings <ben@hoskings.net>
2010-12-21 18:38:06 +1100

    Incomplete work on rvm mirroring.

--- rvm.rb
+++ rvm.rb
@@ -33,7 +33,9 @@ meta :rvm_mirror do
   template {
     requires 'rvm'
     helper :urls do
-      shell("grep '_url=' ~/.rvm/config/db").split("\n").map {|l|
+      shell("grep '_url=' ~/.rvm/config/db").split("\n").reject {|l|
+        l['_repo_url']
+      }.map {|l|
         l.sub(/^.*_url=/, '')
       }
     end
@@ -41,42 +43,23 @@ meta :rvm_mirror do
 end
 
 dep 'mirrored.rvm_mirror' do
-  define_var :rvm_downloads, :default => '/srv/http/files'
   define_var :rvm_vhost_root, :default => '/srv/http/rvm'
   helper :missing_urls do
     urls.tap {|urls| log "#{urls.length} URLs in the rvm database." }.reject {|url|
-      path = var(:rvm_downloads) / File.basename(url)
+      path = var(:rvm_vhost_root) / url.sub(/^[a-z]+:\/\/[^\/]+\//, '')
       path.exists? && !path.empty?
     }.tap {|urls| log "Of those, #{urls.length} aren't present locally." }
   end
   met? { missing_urls.empty? }
   meet {
-    in_dir var(:rvm_downloads) do
-      missing_urls.each {|url|
-        begin
+    missing_urls.each {|url|
+      in_dir(var(:rvm_vhost_root) / File.dirname(url.sub(/^[a-z]+:\/\/[^\/]+\//, '')), :create => true) do
+        # begin
           Babushka::Resource.download url
-        rescue StandardError => ex
-          log_error ex.inspect
-        end
-      }
-    end
-  }
-end
-
-dep 'linked.rvm_mirror' do
-  requires 'mirrored.rvm_mirror'
-  helper :unlinked_urls do
-    urls.tap {|urls| log "#{urls.length} URLs in the rvm download pool." }.select {|url|
-      path = var(:rvm_downloads) / File.basename(url)
-      link = var(:rvm_vhost_root) / url.sub(/^[a-z]+:\/\/[^\/]+\//, '')
-      path.exists? && !(link.exists? && link.readlink)
-    }.tap {|urls| log "Of those, #{urls.length} aren't symlinked into the vhost." }
-  end
-  met? { unlinked_urls.empty? }
-  meet {
-    unlinked_urls.each {|url|
-      shell "mkdir -p '#{var(:rvm_vhost_root) / File.dirname(url.sub(/^[a-z]+:\/\/[^\/]+\//, ''))}'"
-      log_shell "Linking #{url}", "ln -sf '#{var(:rvm_downloads) / File.basename(url)}' '#{var(:rvm_vhost_root) / url.sub(/^[a-z]+:\/\/[^\/]+\//, '')}'"
+        # rescue StandardError => ex
+        #   log_error ex.inspect
+        # end
+      end
     }
   }
   after {
