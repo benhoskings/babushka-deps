b03bf084e3b484f7772f18f60ef9b0d043717e9c
Ben Hoskings <ben@hoskings.net>
2010-12-27 10:52:24 +1100

    Added configure_args to 'ruby trunk.src' as in ruby19.src.

--- ruby.rb
+++ ruby.rb
@@ -2,6 +2,7 @@ dep 'ruby trunk.src' do
   requires 'bison.managed', 'readline headers.managed'
   source 'git://github.com/ruby/ruby.git'
   provides 'ruby == 1.9.3.dev', 'gem', 'irb'
+  configure_args '--disable-install-doc', '--with-readline-dir=/usr'
 end
 
 dep 'ruby19.src' do
