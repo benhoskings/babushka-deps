09aca1789d831b16fcf6435e9fb9afa89c05f8cc
Ben Hoskings <ben@hoskings.net>
2011-01-21 10:16:17 +1100

    Added 'lamp stack removed', for use in specific configs.

---
+++ apache.rb
@@ -0,0 +1,22 @@
+dep 'lamp stack removed', :for => :apt do
+  def packages
+    shell("dpkg --get-selections").select {|l|
+      l[/\binstall$/]
+    }.split("\n").map {|l|
+      l.split(/\s+/, 2).first
+    }.select {|l|
+      l[/apache|mysql|php/]
+    }
+  end
+  met? {
+    packages.empty?
+  }
+  meet {
+    packages.each {|pkg|
+      log_shell "Removing #{pkg}", "apt-get -y remove --purge '#{pkg}'", :sudo => true
+    }
+  }
+  after {
+    log_shell "Autoremoving packages", "apt-get -y autoremove", :sudo => true
+  }
+end
