058c69f36b87f463f3219598bc378df6d9d162df
Ben Hoskings <ben@hoskings.net>
2011-01-21 12:56:49 +1100

    Got 'up to date.deploy_repo' and 'ready for update.deploy_repo' working.

--- deploy.rb
+++ deploy.rb
@@ -1,24 +1,34 @@
-dep 'deploy repo up to date' do
-  setup {
-    # Should look like this:
+meta :deploy_repo do
+  def repo
+    @repo ||= Babushka::GitRepo.new(var(:repo_path))
+  end
+  def ref_data_regexp
+    # For example:
     # 83a90415670ec7ae4690d58563be628c73900716 e817f54d3e9a2d982b16328f8d7f0fbfcd7433f7 refs/heads/master
-    old_id, new_id, branch = var(:git_ref_data).scan(
-      /^([\da-f]{40}) ([\da-f]{40}) refs\/heads\/(.+)$/
-    ).flatten
-    if [old_id, new_id, branch].any?(&:nil?)
-      raise UnmeetableDep, "Invalid value '#{var(:git_ref_data)}' for :git_ref_data."
-    else
-      set :old_id, old_id
-      set :new_id, new_id
-      set :branch, branch
-    end
+    /\A([\da-f]{40}) ([\da-f]{40}) refs\/heads\/(.+)\z/
+  end
+end
+
+dep 'ready for update.deploy_repo' do
+  requires [
+    'valid git_ref_data.deploy_repo',
+    'clean.deploy_repo'
+  ]
+end
+
+dep 'up to date.deploy_repo' do
+  setup {
+    set :rails_root, var(:repo_path)
+    set :rails_env, 'production'
+    set :username, shell('whoami')
+
+    requires Dep('current dir:deployed') # app-specific deps
   }
   requires [
-    'branch name',
-    'clean.repo',
-    'branch exists.repo',
-    'branch checked out.repo',
-    'HEAD up to date.repo',
+    'ref info extracted.deploy_repo',
+    'branch exists.deploy_repo',
+    'branch checked out.deploy_repo',
+    'HEAD up to date.deploy_repo',
     'submodules up to date.task',
     'cached JS and CSS removed',
     'app bundled',
@@ -26,32 +36,44 @@ dep 'deploy repo up to date' do
   ]
 end
 
-dep 'branch name' do
-  
+dep 'ref info extracted.deploy_repo' do
+  requires 'valid git_ref_data.deploy_repo'
+  met? {
+    %w[old_id new_id branch].all? {|name|
+      !Babushka::Base.task.vars.vars[name][:value].nil?
+    }
+  }
+  meet {
+    old_id, new_id, branch = var(:git_ref_data).scan(ref_data_regexp).flatten
+    set :old_id, old_id
+    set :new_id, new_id
+    set :branch, branch
+  }
 end
 
-meta :repo do
-  def repo
-    @repo ||= Babushka::GitRepo.new(var(:repo_path))
-  end
+dep 'valid git_ref_data.deploy_repo' do
+  met? {
+    var(:git_ref_data)[ref_data_regexp] ||
+      raise(UnmeetableDep, "Invalid value '#{var(:git_ref_data)}' for :git_ref_data.")
+  }
 end
 
-dep 'clean.repo' do
-  met? { repo.clean? }
+dep 'clean.deploy_repo' do
+  met? { repo.clean? || raise(UnmeetableDep, "The remote repo has local changes.") }
 end
 
-dep 'branch exists.repo' do
+dep 'branch exists.deploy_repo' do
   met? { repo.branches.include? var(:branch) }
   meet { repo.branch! var(:branch) }
 end
 
-dep 'branch checked out.repo' do
+dep 'branch checked out.deploy_repo' do
   met? { repo.current_branch == var(:branch) }
   meet { repo.checkout! var(:branch) }
 end
 
-dep 'HEAD up to date.repo' do
-  met? { repo.current_head == var(:new_id) }
+dep 'HEAD up to date.deploy_repo' do
+  met? { repo.current_full_head == var(:new_id) && repo.clean? }
   meet { repo.reset_hard! var(:new_id) }
 end
 
