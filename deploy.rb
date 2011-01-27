6b6d0ce3119b97b92ba04b5063f3267dd00c7b55
Ben Hoskings <ben@hoskings.net>
2011-01-27 18:42:09 +1100

    Moved the repo template to its own file.

--- deploy.rb
+++ deploy.rb
@@ -1,14 +1,3 @@
-meta :repo do
-  def repo
-    @repo ||= Babushka::GitRepo.new(var(:repo_path))
-  end
-  def ref_data_regexp
-    # For example:
-    # 83a90415670ec7ae4690d58563be628c73900716 e817f54d3e9a2d982b16328f8d7f0fbfcd7433f7 refs/heads/master
-    /\A([\da-f]{40}) ([\da-f]{40}) refs\/heads\/(.+)\z/
-  end
-end
-
 dep 'ready for update.repo' do
   requires [
     'valid git_ref_data.repo',
