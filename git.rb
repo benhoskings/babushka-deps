1a0e88fbe00a6110c84e813bbb54691cf200ac32
Ben Hoskings <ben@hoskings.net>
2011-03-19 15:03:40 +1100

    Added git.managed.

--- git.rb
+++ git.rb
@@ -1,3 +1,10 @@
+dep 'git.managed' do
+  requires 'git.ppa'
+  installs 'git'
+  provides 'git >= 1.7.4.1'
+end
+
+
 dep 'passenger deploy repo' do
   met? { raise UnmeetableDep, "This dep was renamed to 'web repo'." }
 end
