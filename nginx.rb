eb06fdb8955217bf968cf0947a525dfbd093553b
Ben Hoskings <ben@hoskings.net>
2011-03-22 15:06:24 +1100

    We build passenger, not buildl it.

--- nginx.rb
+++ nginx.rb
@@ -153,7 +153,7 @@ dep 'passenger built' do
   }
   meet {
     in_dir Babushka::GemHelper.gem_path_for('passenger') do
-      log_shell "Buildling passenger", "rake clean nginx", :sudo => Babushka::GemHelper.should_sudo?
+      log_shell "Building passenger", "rake clean nginx", :sudo => Babushka::GemHelper.should_sudo?
     end
   }
 end
