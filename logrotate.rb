3527f2a44308ce37cb3792d834e0932846e9ca25
Ben Hoskings <ben@hoskings.net>
2011-03-29 14:59:45 +1100

    Added draft logrotate deps.

---
+++ logrotate.rb
@@ -0,0 +1,26 @@
+meta :logrotate do
+  accepts_value_for :renders
+  accepts_value_for :as
+  template {
+    def conf_dest
+      "/etc/logrotate.d/#{as}"
+    end
+    requires 'logrotate.managed'
+    met? {
+      Babushka::Renderable.new(conf_dest).from?("logrotate/#{renders}")
+    }
+    meet {
+      render_erb "logrotate/#{renders}", :to => conf_dest, :sudo => true
+    }
+  }
+end
+
+dep 'nginx.logrotate' do
+  renders "logrotate/nginx.conf"
+  as "nginx"
+end
+
+dep 'rails.logrotate' do
+  renders "logrotate/rails.conf"
+  as var(:domain)
+end
