6c843da46e70f66c7cdfa73cf38a398a28bb77dd
Ben Hoskings <ben@hoskings.net>
2011-03-29 16:24:01 +1100

    Detect the logrotate.d location in /etc or /usr/local/etc.

--- logrotate.rb
+++ logrotate.rb
@@ -2,10 +2,15 @@ meta :logrotate do
   accepts_value_for :renders
   accepts_value_for :as
   template {
-    def conf_dest
-      "/etc/logrotate.d/#{as}"
-    end
     requires 'logrotate.managed'
+    def conf_dest
+      %w[
+        /usr/local/etc/logrotate.d
+        /etc/logrotate.d
+      ].detect {|path|
+        path.p.exists?
+      } / as
+    end
     met? {
       Babushka::Renderable.new(conf_dest).from?(dependency.load_path.parent / renders)
     }
