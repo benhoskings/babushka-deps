545534a4d552cae0ee5ecd7b66627ec143be2759
Ben Hoskings <ben@hoskings.net>
2011-05-27 15:43:52 +1000

    Cache the remote head before and after the push.

--- push.rb
+++ push.rb
@@ -2,12 +2,18 @@ meta :push do
   def repo
     @repo ||= Babushka::GitRepo.new('.')
   end
-  def remote_location remote_name
-    shell("git config remote.#{remote_name}.url").split(':', 2)
+  def remote_location
+    shell("git config remote.#{var(:remote)}.url").split(':', 2)
   end
-  def remote_head remote_name
-    host, path = remote_location(remote_name)
-    shell "ssh #{host} 'cd #{path} && git rev-parse --short HEAD 2>/dev/null'"
+  def remote_head
+    host, path = remote_location
+    @@remote_head ||= shell("ssh #{host} 'cd #{path} && git rev-parse --short HEAD 2>/dev/null'")
+  end
+  def prev_head
+    @@prev_head
+  end
+  def uncache_remote_head!
+    @@prev_head, @@remote_head = @@remote_head, nil
   end
   def git_log from, to
     log shell("git log --graph --pretty=format:'%Cblue%h%d%Creset %ad %Cgreen%an%Creset %s' #{from}..#{to}")
@@ -55,16 +61,16 @@ dep 'pushed.push' do
     'ok to update.push'
   ]
   met? {
-    @remote_head = remote_head(var(:remote))
-    (@remote_head == shell("git rev-parse --short #{var(:ref)} 2>/dev/null")).tap {|result|
-      log "#{var(:remote)} is on #{@remote_head}.", :as => (:ok if result)
+    (remote_head == shell("git rev-parse --short #{var(:ref)} 2>/dev/null")).tap {|result|
+      log "#{var(:remote)} is on #{remote_head}.", :as => (:ok if result)
     }
   }
   meet {
-    git_log @remote_head, var(:ref)
+    git_log remote_head, var(:ref)
     confirm "OK to push to #{var(:remote)} (#{repo.repo_shell("git config remote.#{var(:remote)}.url")})?" do
       push_cmd = "git push #{var(:remote)} #{var(:ref)}:babs -f"
       log push_cmd.colorize("on grey") do
+        uncache_remote_head!
         shell push_cmd, :log => true
       end
     end
@@ -77,14 +83,13 @@ dep 'tagged.push' do
     @current_tag == repo.repo_shell("git rev-parse #{var(:ref)}")
   }
   meet {
-    repo.repo_shell "git tag -f prev-#{var(:remote)} on-#{var(:remote)}" unless @current_tag.nil?
+    repo.repo_shell "git tag -f prev-#{var(:remote)} #{prev_head}" unless @current_tag.nil?
     repo.repo_shell "git tag -f on-#{var(:remote)} #{var(:ref)}"
   }
 end
 
 dep 'ok to update.push' do
   met? {
-    remote_head = remote_head(var(:remote))
     if !repo.repo_shell("git rev-parse #{remote_head}", &:ok?)
       confirm "The current HEAD on #{var(:remote)}, #{remote_head}, isn't present locally. OK to push #{'(This is probably a bad idea)'.colorize('on red')}"
     elsif shell("git merge-base #{var(:ref)} #{remote_head}")[0...7] != remote_head
