7679cd12540cf665a91324b74dcdc8d0fd8b7d0c
Ben Hoskings <ben@hoskings.net>
2011-06-02 17:13:58 +1000

    Allow refs/tags/, etc, to be pushed to a web repo as well as refs/heads/.

--- templates/repo.rb
+++ templates/repo.rb
@@ -4,6 +4,6 @@ meta :repo do
   end
   def ref_data_regexp
     # Example: "83a90415670ec7ae4690d58563be628c73900716 e817f54d3e9a2d982b16328f8d7f0fbfcd7433f7 refs/heads/master"
-    /\A([\da-f]{40}) ([\da-f]{40}) refs\/heads\/(.+)\z/
+    /\A([\da-f]{40}) ([\da-f]{40}) refs\/[^\/]+\/(.+)\z/
   end
 end
