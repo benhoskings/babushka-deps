64f764a196dc9d22a97d9aaa8292a4ca6f4e4abe
Ben Hoskings <ben@hoskings.net>
2011-08-16 19:55:21 +1000

    Fixed 'app bundled' when there's no Gemfile.

--- bundler.rb
+++ bundler.rb
@@ -1,7 +1,13 @@
 dep 'app bundled' do
-  requires 'Gemfile'
   requires_when_unmet Dep('current dir:packages')
-  met? { cd(var(:app_root)) { shell? 'bundle check', :log => true } }
+  met? {
+    if !(var(:app_root) / 'Gemfile').exists?
+      log "No Gemfile - skipping bundling."
+      true
+    else
+      cd(var(:app_root)) { shell? 'bundle check', :log => true }
+    end
+  }
   meet { cd(var(:app_root)) {
     install_args = var(:app_env) != 'production' ? '' : "--deployment --without 'development test'"
     unless shell("bundle install #{install_args}", :log => true)
@@ -10,8 +16,4 @@ dep 'app bundled' do
       }
     end
   } }
-end
-
-dep 'Gemfile' do
-  met? { (var(:app_root) / 'Gemfile').exists? }
 end
