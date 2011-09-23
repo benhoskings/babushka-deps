db2639494d289071f6e40f58d6e042debee7992d
Ben Hoskings <ben@hoskings.net>
2011-09-23 12:04:42 +1000

    Updated push! and related deps to use dep params.

--- push.rb
+++ push.rb
@@ -2,15 +2,12 @@ meta :push do
   def repo
     @repo ||= Babushka::GitRepo.new('.')
   end
-  def self.remote_location
-    shell("git config remote.#{var(:remote)}.url").split(':', 2)
-  end
-  def self.remote_head
-    host, path = remote_location
-    @remote_head ||= shell("ssh #{host} 'cd #{path} && git rev-parse --short HEAD 2>/dev/null'") || ''
+  def self.remote_head remote
+    host, path = shell("git config remote.#{remote}.url").split(':', 2)
+    @remote_head ||= shell!("ssh #{host} 'cd #{path} && git rev-parse --short HEAD 2>/dev/null'") || ''
   end
   def remote_head
-    self.class.remote_head
+    self.class.remote_head(remote)
   end
   def self.uncache_remote_head!
     @remote_head = nil
@@ -20,13 +17,13 @@ meta :push do
   end
 end
 
-dep 'push!' do
-  define_var :ref, :message => "What would you like to push?", :default => 'HEAD'
+dep 'push!', :ref do
+  ref.ask("What would you like to push?").default('HEAD')
   requires [
     'ready.push',
     'before push',
-    'pushed.push',
-    'marked on newrelic.task',
+    'pushed.push'.with(:ref => ref),
+    'marked on newrelic.task'.with(ref),
     'after push'
   ]
 end
@@ -50,23 +47,21 @@ dep 'ready.push' do
   }
 end
 
-dep 'pushed.push' do
-  define_var :remote,
-    :message => "Where would you like to push to?",
-    :choices => repo.repo_shell('git remote').split("\n")
+dep 'pushed.push', :ref, :remote do
+  remote.ask("Where would you like to push to?").choose(repo.repo_shell('git remote').split("\n"))
   requires [
-    'on origin.push',
-    'ok to update.push'
+    'on origin.push'.with(ref),
+    'ok to update.push'.with(ref, remote)
   ]
   met? {
-    (remote_head == shell("git rev-parse --short #{var(:ref)} 2>/dev/null")).tap {|result|
-      log "#{var(:remote)} is on #{remote_head}.", :as => (:ok if result)
+    (remote_head == shell("git rev-parse --short #{ref} 2>/dev/null")).tap {|result|
+      log "#{remote} is on #{remote_head}.", :as => (:ok if result)
     }
   }
   meet {
-    git_log remote_head, var(:ref)
-    confirm "OK to push to #{var(:remote)} (#{repo.repo_shell("git config remote.#{var(:remote)}.url")})?" do
-      push_cmd = "git push #{var(:remote)} #{var(:ref)}:master -f"
+    git_log remote_head, ref
+    confirm "OK to push to #{remote} (#{repo.repo_shell("git config remote.#{remote}.url")})?" do
+      push_cmd = "git push #{remote} #{ref}:master -f"
       log push_cmd.colorize("on grey") do
         self.class.uncache_remote_head!
         shell push_cmd, :log => true
@@ -75,40 +70,40 @@ dep 'pushed.push' do
   }
 end
 
-dep 'marked on newrelic.task' do
+dep 'marked on newrelic.task', :ref do
   run {
     if 'config/newrelic.yml'.p.exists?
-      shell "bundle exec newrelic deployments -r #{shell("git rev-parse --short #{var(:ref)}")}"
+      shell "bundle exec newrelic deployments -r #{shell("git rev-parse --short #{ref}")}"
     end
   }
 end
 
-dep 'ok to update.push' do
+dep 'ok to update.push', :ref, :remote do
   met? {
     if !repo.repo_shell("git rev-parse #{remote_head}", &:ok?)
-      confirm "The current HEAD on #{var(:remote)}, #{remote_head}, isn't present locally. OK to push #{'(This is probably a bad idea)'.colorize('on red')}"
-    elsif shell("git merge-base #{var(:ref)} #{remote_head}", &:stdout)[0...7] != remote_head
-      confirm "Pushing #{var(:ref)} to #{var(:remote)} would not fast forward (#{var(:remote)} is on #{remote_head}). That OK?"
+      confirm "The current HEAD on #{remote}, #{remote_head}, isn't present locally. OK to push #{'(This is probably a bad idea)'.colorize('on red')}"
+    elsif shell("git merge-base #{ref} #{remote_head}", &:stdout)[0...7] != remote_head
+      confirm "Pushing #{ref} to #{remote} would not fast forward (#{remote} is on #{remote_head}). That OK?"
     else
       true
     end
   }
 end
 
-dep 'on origin.push' do
-  requires Dep('remote exists.push', :from => dependency.dep_source).with('origin')
+dep 'on origin.push', :ref do
+  requires 'remote exists.push'.with('origin')
   met? {
-    shell("git branch -r --contains #{var(:ref)}").split("\n").map(&:strip).include? "origin/#{repo.current_branch}"
+    shell("git branch -r --contains #{ref}").split("\n").map(&:strip).include? "origin/#{repo.current_branch}"
   }
   meet {
-    git_log "origin/#{repo.current_branch}", var(:ref)
-    confirm("#{var(:ref)} isn't pushed to origin/#{repo.current_branch} yet. Do that now?") do
+    git_log "origin/#{repo.current_branch}", ref
+    confirm("#{ref} isn't pushed to origin/#{repo.current_branch} yet. Do that now?") do
       shell("git push origin #{repo.current_branch}")
     end
   }
 end
 
-dep 'remote exists.push' do |remote|
+dep 'remote exists.push', :remote do
   met? {
     repo.repo_shell("git config remote.#{remote}.url") or log_error("The #{remote} remote isn't configured.")
   }
