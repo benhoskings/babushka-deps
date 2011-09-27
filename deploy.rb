7d635a2fed4f9468c3f1ce59fc65988307ad60be
Ben Hoskings <ben@hoskings.net>
2011-09-27 13:53:31 +1000

    Make the environment into a parameter for the deploy deps instead of hardcoding it to 'production', passing it though to the app-specific 'on deploy' and 'after deploy'.

--- deploy.rb
+++ deploy.rb
@@ -7,15 +7,16 @@ dep 'ready for update.repo', :git_ref_data do
   ]
 end
 
-dep 'up to date.repo', :git_ref_data do
+dep 'up to date.repo', :git_ref_data, :env do
   def ref_info
     old_id, new_id, branch = git_ref_data.to_s.scan(ref_data_regexp).flatten
     {:old_id => old_id, :new_id => new_id, :branch => branch}
   end
+  env.default!('production')
   requires [
     'on correct branch.repo'.with(ref_info[:branch]),
     'HEAD up to date.repo'.with(ref_info),
-    'app bundled'.with(:root => '.', :env => 'production'),
+    'app bundled'.with(:root => '.', :env => env),
 
     # This and the 'maintenace' one below are separate so the 'current dir'
     # deps load lazily from the new code checked out by 'HEAD up to date.repo'.
@@ -30,11 +31,11 @@ dep 'up to date.repo', :git_ref_data do
 end
 
 # These are looked up with Dep() so they're just skipped if they don't exist.
-dep 'on deploy', :old_id, :new_id, :branch do
-  requires 'current dir:on deploy'.with(old_id, new_id, branch) if Dep('current dir:on deploy')
+dep 'on deploy', :old_id, :new_id, :branch, :env do
+  requires 'current dir:on deploy'.with(old_id, new_id, branch, env) if Dep('current dir:on deploy')
 end
-dep 'after deploy', :old_id, :new_id, :branch do
-  requires 'current dir:after deploy'.with(old_id, new_id, branch) if Dep('current dir:after deploy')
+dep 'after deploy', :old_id, :new_id, :branch, :env do
+  requires 'current dir:after deploy'.with(old_id, new_id, branch, env) if Dep('current dir:after deploy')
 end
 
 dep 'valid git_ref_data.repo', :git_ref_data do
