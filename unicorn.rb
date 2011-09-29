f00e5737964b8114ef04a31b6fad8911c912b42b
Ben Hoskings <ben@hoskings.net>
2011-09-29 11:46:40 +1000

    Parameterised the unicorn deps.

--- unicorn.rb
+++ unicorn.rb
@@ -1,14 +1,14 @@
-dep 'unicorn configured' do
-  requires 'unicorn.gem', 'unicorn config exists.nginx'
+dep 'unicorn configured', :path do
+  requires 'unicorn.gem', 'unicorn config exists.nginx'.with(path)
 end
 
 dep 'unicorn.gem' do
   provides %w[unicorn unicorn_rails]
 end
 
-dep 'unicorn config exists.nginx' do
+dep 'unicorn config exists.nginx', :path do
   def unicorn_config
-    var(:app_root) / 'config/unicorn.rb'
+    path / 'config/unicorn.rb'
   end
   met? { Babushka::Renderable.new(unicorn_config).from?(dependency.load_path.parent / "unicorn/unicorn.rb.erb") }
   meet { render_erb 'unicorn/unicorn.rb.erb', :to => unicorn_config }
