538de89001408aa624fa695a07d875fb389b66e9
Ben Hoskings <ben@hoskings.net>
2011-10-02 17:18:43 +1100

    Redesigned 'deployed migrations run' to use a setup{} block, instead of the run-once hack.

--- db.rb
+++ db.rb
@@ -8,27 +8,19 @@ dep 'db gem', :db do
 end
 
 dep 'deployed migrations run', :old_id, :new_id, :env do
-  requires_when_unmet 'maintenance page up'
-  requires_when_unmet 'migrated db'.with(shell('whoami'), '.', env, 'yes')
-  met? {
-    if @run
-      true # done
+  setup {
+    # If the branch was changed, git supplies 0000000 for old_id,
+    # so the commit range is 'everything'.
+    effective_old_id = old_id[/^0+$/] ? '' : old_id
+    pending = shell("git diff --numstat #{effective_old_id}..#{new_id}").split("\n").grep(/^[\d\s]+db\/migrate\//)
+    if pending.empty?
+      log "No new migrations."
     else
-      # If the branch was changed, git supplies 0000000 for old_id,
-      # so the commit range is 'everything'.
-      effective_old_id = old_id[/^0+$/] ? '' : old_id
-      pending = shell("git diff --numstat #{effective_old_id}..#{new_id}").split("\n").grep(/^[\d\s]+db\/migrate\//)
-      if pending.empty?
-        log "No new migrations."
-        true
-      else
-        log "#{pending.length} migration#{'s' unless pending.length == 1} to run:"
-        pending.each {|p| log p }
-        false
-      end
+      log "#{pending.length} migration#{'s' unless pending.length == 1} to run:"
+      pending.each {|p| log p }
+
+      requires 'maintenance page up'
+      requires 'migrated db'.with(shell('whoami'), '.', env, 'yes')
     end
-  }
-  meet {
-    @run = true
   }
 end
