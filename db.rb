ec65052a3a094e3b4d29674c3830371cf4d75d62
Ben Hoskings <ben@hoskings.net>
2011-10-02 15:57:26 +1100

    defer to 'migrated db' in the renamed 'deployed migrations run' instead of migrating locally as a task. It can pick datamapper/activerecord now, and on activerecord, is a proper dep with a met? check.

--- db.rb
+++ db.rb
@@ -11,14 +11,15 @@ dep 'db gem' do
   }
 end
 
-dep 'activerecord db migrated', :old_id, :new_id, :env do
-  requires_when_unmet 'benhoskings:maintenance page up'
+dep 'deployed migrations run', :old_id, :new_id, :env do
+  requires_when_unmet 'maintenance page up'
+  requires_when_unmet 'migrated db'.with(shell('whoami'), '.', env)
   met? {
     if @run
       true # done
     else
-      # If the branch was changed, git supplies 0000000 for var(:old_id), so
-      # it looks the commit range is 'everything'.
+      # If the branch was changed, git supplies 0000000 for old_id,
+      # so the commit range is 'everything'.
       effective_old_id = old_id[/^0+$/] ? '' : old_id
       pending = shell("git diff --numstat #{effective_old_id}..#{new_id}").split("\n").grep(/^[\d\s]+db\/migrate\//)
       if pending.empty?
@@ -32,7 +33,6 @@ dep 'activerecord db migrated', :old_id, :new_id, :env do
     end
   }
   meet {
-    shell "bundle exec rake db:migrate --trace RAILS_ENV=#{env}", :log => true
     @run = true
   }
 end
