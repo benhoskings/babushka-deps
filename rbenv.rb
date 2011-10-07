a5f69a70e3cc9f602c74b2793723080461309517
Ben Hoskings <ben@hoskings.net>
2011-10-07 15:30:53 +1100

    Updated the rbenv docs.

--- rbenv.rb
+++ rbenv.rb
@@ -1,7 +1,7 @@
 # Usage:
 # [install rbenv like normal] https://github.com/sstephenson/rbenv
 # babushka 1.9.3.rbenv
-# rbenv set-default 1.9.3-preview1
+# rbenv global 1.9.3-rc1
 # rbenv rehash
 
 dep 'rbenv' do
