244a240e7271664e9b1a6ca29f2eed71be1b5714
Ben Hoskings <ben@hoskings.net>
2012-01-10 10:42:21 +1100

    nginx-1.0.11.

--- nginx.rb
+++ nginx.rb
@@ -134,7 +134,7 @@ end
 
 dep 'nginx.src', :nginx_prefix, :version, :upload_module_version do
   nginx_prefix.default!("/opt/nginx")
-  version.default!('1.0.10')
+  version.default!('1.0.11')
   upload_module_version.default!('2.2.0')
   requires 'pcre.managed', 'libssl headers.managed', 'zlib headers.managed'
   source "http://nginx.org/download/nginx-#{version}.tar.gz"
