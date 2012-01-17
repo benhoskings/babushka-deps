e2339485f444260f580e8cada9d38454afc31221
Ben Hoskings <ben@hoskings.net>
2012-01-18 09:52:13 +1100

    Don't build assets on deploy by default: that's not common to all deploys.

--- deploy.rb
+++ deploy.rb
@@ -22,8 +22,6 @@ dep 'up to date.repo', :git_ref_data, :env do
     # checks trigger a source load when called.
     'on deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env),
 
-    '☕',
-    'scss built',
     'app flagged for restart.task',
     'maintenance page down',
     'after deploy'.with(ref_info[:old_id], ref_info[:new_id], ref_info[:branch], env)
