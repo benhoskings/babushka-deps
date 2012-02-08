000b782fb3d355c8217813b6433cdef9ab659155
Ben Hoskings <ben@hoskings.net>
2012-02-08 11:28:05 +1100

    This was never going to work. Revert "Only update the schema after pushing to production, not other envs."
    This reverts commit 262c70e9ce13fb28c8fa9617c07100f117a40c58.
    
    Conflicts:
    
    	push.rb

--- push.rb
+++ push.rb
@@ -30,6 +30,7 @@ dep 'push!', :ref, :remote do
   requires 'ready.push'
   requires 'current dir:before push'.with(ref, remote) if Dep('current dir:before push')
   requires 'pushed.push'.with(ref, remote)
+  requires 'schema up to date.push'.with(ref, remote)
   requires 'marked on newrelic.task'.with(ref, remote)
   requires 'marked on airbrake.task'.with(ref, remote)
   requires 'current dir:after push'.with(ref, remote) if Dep('current dir:after push')
@@ -66,9 +67,6 @@ dep 'pushed.push', :ref, :remote do
         shell push_cmd, :log => true
       end
     end
-  }
-  after {
-    Dep('benhoskings:schema up to date.push').with(ref, remote).meet if remote == 'production'
   }
 end
 
