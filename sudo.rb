44bc0ba445eebfb61bc933455c67f0be707b2c4d
Ben Hoskings <ben@hoskings.net>
2012-02-24 17:14:12 +1100

    In 'passwordless sudo removed', remove all passwordless sudos, not just one user's.

--- sudo.rb
+++ sudo.rb
@@ -11,16 +11,14 @@ dep 'passwordless sudo', :username do
   }
 end
 
-dep 'passwordless sudo removed', :username do
+dep 'passwordless sudo removed' do
   setup {
     unmeetable! "This dep must be run as root." unless shell('whoami') == 'root'
   }
   met? {
-    shell 'sudo -k', :as => 'ben' # expire an existing cached password
-    raw_shell('sudo -n true', :as => 'ben').stderr['sorry, a password is required to run sudo']
+    raw_shell('grep NOPASSWD /etc/sudoers').stdout.empty?
   }
   meet {
-    # We have to use a trailing space instead of \b because BSD sed is nub.
-    shell "sed -i '' '/^#{Regexp.escape(username)} /d' /etc/sudoers"
+    shell "sed -i '' '/NOPASSWD/d' /etc/sudoers"
   }
 end
