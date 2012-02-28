ece2a5be4496090a1b70bd7d5319d60f31655831
Ben Hoskings <ben@hoskings.net>
2012-02-28 15:10:50 +1100

    Only require the postgres ppa when postgres isn't installed.

--- postgres.rb
+++ postgres.rb
@@ -50,9 +50,9 @@ dep 'postgres.managed', :version do
   version.default('9.1')
   # Assume the installed version if there is one
   version.default!(shell('psql --version').val_for('psql (PostgreSQL)')[/^\d\.\d/]) if which('psql')
-  requires {
-    on :apt, 'set.locale', 'ppa'.with('ppa:pitti/postgresql')
-    on :brew, 'set.locale'
+  requires 'set.locale'
+  requires_when_unmet {
+    on :apt, 'ppa'.with('ppa:pitti/postgresql')
   }
   installs {
     via :apt, ["postgresql-#{owner.version}", "libpq-dev"]
