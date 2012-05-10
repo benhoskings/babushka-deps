6f3f03c3b1813a48cae04dcf07ca11ee291aaf4c
Ben Hoskings <ben@hoskings.net>
2012-05-11 09:32:04 +1000

    Move non-core push deps into their own file.

--- push.rb
+++ push.rb
@@ -72,52 +72,6 @@ dep 'pushed.push', :ref, :remote do
   }
 end
 
-dep 'schema up to date.push', :ref, :remote, :env do
-  def db_name
-    'config/database.yml'.p.yaml[env.to_s]['database']
-  end
-  def dump_schema_cmd
-    pg_dump = "pg_dump #{db_name} --no-privileges --no-owner"
-    # Dump the schema, and then the schema_migrations table including its contents.
-    "#{pg_dump} --schema-only -T schema_migrations && #{pg_dump} -t schema_migrations"
-  end
-  def fetch_schema
-    shell "ssh #{remote_host} '#{dump_schema_cmd}' > db/schema.sql.tmp"
-  end
-  def move_schema_into_place
-    shell "mv db/schema.sql.tmp db/schema.sql"
-  end
-  setup {
-    # We fetch to a temporary file first and move it into place on ssh
-    # success, because a failed connection can result in an empty file.
-    fetch_schema and move_schema_into_place
-  }
-  met? {
-    Babushka::GitRepo.new('.').clean?
-  }
-  meet {
-    shell "git add db/schema.sql && git commit db/schema.sql -m 'Update DB schema after deploying #{shell("git rev-parse --short #{ref}")}.'"
-  }
-end
-
-dep 'marked on newrelic.task', :ref, :env do
-  requires 'app bundled'.with('.', 'development')
-  run {
-    if 'config/newrelic.yml'.p.exists?
-      shell "bundle exec newrelic deployments -e #{env} -r #{shell("git rev-parse --short #{ref}")}"
-    end
-  }
-end
-
-dep 'marked on airbrake.task', :ref, :env do
-  requires 'app bundled'.with('.', 'development')
-  run {
-    if 'config/initializers/airbrake.rb'.p.exists?
-      shell "bundle exec rake airbrake:deploy TO=#{env} REVISION=#{shell("git rev-parse --short #{ref}")} REPO=#{shell("git config remote.origin.url")} USER=#{shell('whoami')}"
-    end
-  }
-end
-
 dep 'ok to update.push', :ref, :remote do
   met? {
     if remote_head[/^0+$/]
