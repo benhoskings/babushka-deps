96de2573d9912476778703d76b3dc68de62b81fd
Ben Hoskings <ben@hoskings.net>
2012-05-13 17:43:08 +1000

    A File.exists? -> "".p.exists? refactor.

--- user.rb
+++ user.rb
@@ -1,7 +1,7 @@
 dep 'dot files', :username do
   username.default!(shell('whoami'))
   requires 'user exists'.with(:username => username), 'git', 'curl.managed', 'git-smart.gem'
-  met? { File.exists?(ENV['HOME'] / ".dot-files/.git") }
+  met? { "~/.dot-files/.git".p.exists? }
   meet { shell %Q{curl -L "http://github.com/#{var :github_user, :default => 'benhoskings'}/#{var :dot_files_repo, :default => 'dot-files'}/raw/master/clone_and_link.sh" | bash} }
 end
 
