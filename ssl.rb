f7dc8375bae0814ecea218f94a68c89e17ca3f97
Ben Hoskings <ben@hoskings.net>
2012-07-11 13:50:25 +1000

    Remove #append_to_file call from 'passwordless ssh logins'.

--- ssl.rb
+++ ssl.rb
@@ -14,7 +14,7 @@ dep 'passwordless ssh logins', :username, :key do
   }
   meet {
     shell "mkdir -p -m 700 '#{ssh_dir}'", :sudo => sudo?
-    append_to_file key, (ssh_dir / 'authorized_keys'), :sudo => sudo?
+    shell "cat >> #{ssh_dir / 'authorized_keys'}", :input => key, :sudo => sudo?
     sudo "chown -R #{username}:#{group} '#{ssh_dir}'" unless ssh_dir.owner == username
     shell "chmod 600 #{(ssh_dir / 'authorized_keys')}", :sudo => sudo?
   }
