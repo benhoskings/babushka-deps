7ee1cf480575729c6c2d0dd67a90c6391d3f1dc1
Ben Hoskings <ben@hoskings.net>
2012-07-11 14:56:34 +1000

    Rewrite ngircd deps to use parameters.

--- irc.rb
+++ irc.rb
@@ -1,4 +1,17 @@
-dep 'ngircd.managed' do
-  installs { via :apt, 'ngircd' }
-  cfg '/etc/ngircd/ngircd.conf'
+dep 'ngircd', :host, :description, :admin_email, :channels do
+  host.default("irc.#{shell('hostname -f')}")
+  description.default("#{hostname} IRC server")
+  admin_email.default("root@#{shell('hostname -f')}")
+  channels.ask("Channel names (space-separated, don't worry about the '#')")
+
+  requires 'ngircd.bin'
+
+  met? {
+    Babushka::Renderable.new().from?(dependency.load_path.parent / "nginx/vhost.conf.erb")
+  }
+  meet {
+    render_erb "nginx/vhost.conf.erb", :to => vhost_conf, :sudo => true
+  }
 end
+
+dep 'ngircd.bin'
