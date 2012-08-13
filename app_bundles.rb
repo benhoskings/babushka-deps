dc748dbb33010734edebb9eb90667c46236a94b1
Ben Hoskings <ben@hoskings.net>
2012-08-13 16:10:55 +1000

    Add 'Sublime Text 2.app'.

--- app_bundles.rb
+++ app_bundles.rb
@@ -1,3 +1,8 @@
+dep 'Sublime Text 2.app' do
+  source 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg'
+  version '>= 2.0.1'
+end
+
 dep 'Vim.app' do
   source 'http://macvim.org/OSX/files/binaries/OSX10_4/Vim7.0-univ.tar.bz2'
 end
