b1d9b83d09b1264fdb6fea229889a189eeaf4429
Ben Hoskings <ben@hoskings.net>
2012-09-10 13:57:36 +1000

    Don't install postgres from pitti/postgresql -- it's too unstable (and 12.04 has 9.1 anyway).

--- postgres.rb
+++ postgres.rb
@@ -51,9 +51,6 @@ dep 'postgres.managed', :version do
   # Assume the installed version if there is one
   version.default!(shell('psql --version').val_for('psql (PostgreSQL)')[/^\d\.\d/]) if which('psql')
   requires 'set.locale'
-  requires_when_unmet {
-    on :apt, 'ppa'.with('ppa:pitti/postgresql')
-  }
   installs {
     via :apt, ["postgresql-#{owner.version}", "libpq-dev"]
     via :brew, "postgresql"
