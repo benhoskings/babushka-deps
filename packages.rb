259c0217ad1a265f6f545a25110225c7b4c34828
Ben Hoskings <ben@hoskings.net>
2012-09-25 16:31:19 +1000

    Formatting.

--- packages.rb
+++ packages.rb
@@ -64,7 +64,10 @@ end
 dep 'libxml.managed' do
   installs {
     # The latest libxml2 on 12.04 doesn't have a corresponding libxml2-dev.
-    on :precise, 'libxml2=2.7.8.dfsg-5.1ubuntu4', 'libxml2-dev=2.7.8.dfsg-5.1ubuntu4'
+    on :precise, [
+      'libxml2 == 2.7.8.dfsg-5.1ubuntu4',
+      'libxml2-dev == 2.7.8.dfsg-5.1ubuntu4'
+    ]
 
     via :apt, 'libxml2-dev'
   }
