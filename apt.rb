d311a726ceb35a8f5456130d5ed23258267604ab
Ben Hoskings <ben@hoskings.net>
2012-10-25 19:18:22 +1100

    Remove 'apt packages removed' wrapper deps.

--- apt.rb
+++ apt.rb
@@ -21,13 +21,3 @@ dep 'apt packages removed', :match, :for => :apt do
     log_shell "Autoremoving packages", "apt-get -y autoremove", :sudo => true
   }
 end
-
-# These are for use in dollhouse, since at the moment only strings can be passed.
-
-dep 'lamp stack removed' do
-  requires 'apt packages removed'.with(/apache|mysql|php/)
-end
-
-dep 'postfix removed' do
-  requires 'apt packages removed'.with(/postfix/)
-end
