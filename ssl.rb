2a9881b54bc7f6e5bb6e208502be40e79f0f807f
Phil Pirozhkov <pirj@mail.ru>
2012-11-01 02:34:46 +0400

    Check if authorized_keys belongs to user (root when first created)

--- ssl.rb
+++ ssl.rb
@@ -16,6 +16,7 @@ dep 'passwordless ssh logins', :username, :key do
     shell "mkdir -p -m 700 '#{ssh_dir}'", :sudo => sudo?
     shell "cat >> #{ssh_dir / 'authorized_keys'}", :input => key, :sudo => sudo?
     sudo "chown -R #{username}:#{group} '#{ssh_dir}'" unless ssh_dir.owner == username
+    sudo "chown -R #{username}:#{group} '#{ssh_dir / 'authorized_keys'}'" unless (ssh_dir / 'authorized_keys').owner == username
     shell "chmod 600 #{(ssh_dir / 'authorized_keys')}", :sudo => sudo?
   }
 end
