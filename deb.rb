6ed52800421ffec13aa1dfdcda0fc936117835b7
Ben Hoskings <ben@hoskings.net>
2013-01-27 15:21:01 +1100

    Update ree.src -> ree to use Resource.get instead of the old #process_source.

--- deb.rb
+++ deb.rb
@@ -1,7 +1,11 @@
-dep 'ree.src' do
-  source "http://rubyforge.org/frs/download.php/71098/ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb"
-  process_source {
-    sudo("dpkg -i ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb")
+dep 'ree' do
+  def source
+    "http://rubyforge.org/frs/download.php/71098/ruby-enterprise_1.8.7-2010.02_amd64_ubuntu10.04.deb"
+  end
+  met? { shell("ruby --version")["ruby 1.8.7 (2010-04-19 patchlevel 253) [x86_64-linux], MBARI 0x6770, Ruby Enterprise Edition 2010.02"] }
+  meet {
+    Babushka::Resource.get(source) {|path|
+      sudo("dpkg -i #{path}")
+    }
   }
-  met? { shell("ruby --version") =~ /#{Regexp.escape("ruby 1.8.7 (2010-04-19 patchlevel 253) [x86_64-linux], MBARI 0x6770, Ruby Enterprise Edition 2010.02")}/ }
 end
