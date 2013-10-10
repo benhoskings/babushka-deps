c8f27abe18f4289f046af2ab147a481ce3b2e3e4
Ben Hoskings <ben@hoskings.net>
2013-10-10 17:44:31 +1100

    Add readline and yaml to ruby.src's configuration properly.

--- ruby.rb
+++ ruby.rb
@@ -9,6 +9,9 @@ dep 'ruby.src', :version, :patchlevel do
   def version_group
     version.to_s.scan(/^\d\.\d/).first
   end
+  def add_extension ext_name
+    log_shell "configure #{ext_name}", "ruby extconf.rb", :cd => "ext/#{ext_name}"
+  end
   version.default!('2.0.0')
   patchlevel.default!('p247')
   requires_when_unmet [
@@ -20,9 +23,11 @@ dep 'ruby.src', :version, :patchlevel do
   ]
   source "ftp://ftp.ruby-lang.org/pub/ruby/#{version_group}/ruby-#{version}-#{patchlevel}.tar.gz"
   provides "ruby == #{version}#{patchlevel}", 'gem', 'irb'
-  configure_args '--disable-install-doc',
-    "--with-readline-dir=#{Babushka.host.pkg_helper.prefix}",
-    "--with-libyaml-dir=#{Babushka.host.pkg_helper.prefix}"
+  configure {
+    log_shell "configure", "./configure --prefix=#{prefix} --disable-install-doc"
+    add_extension 'psych'
+    add_extension 'readline'
+  }
   build {
     log_shell "build", "make -j#{Babushka.host.cpus}"
   }
